2a5eab2c91bc2fcc6ff7b9c4121c12a57946c5ef
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-08-10 16:56:04 +0600

    testing out using lmstudio models

--- run_lmstudio_model.py
+++ run_lmstudio_model.py
@@ -1,26 +1,50 @@
-"""
-Didn't work, LM Studio specific models requires llama-cpp/llama-cpp-python for inference.
-"""
+import base64
+from PyPDF2 import PdfReader
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain_core.runnables.passthrough import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from tiktoken import get_encoding
 
-import torch
-from langchain.prompts import PromptTemplate
-from transformers import AutoTokenizer, AutoModelForCausalLM
-from transformers.pipelines.text_generation import TextGenerationPipeline
-from langchain_huggingface import HuggingFacePipeline
+encoder = get_encoding("o200k_base")
 
-if torch.mps.is_available():
-    print("MPS available")
 
-device = torch.device("mps")
-model_path = "/Users/sifatul/.lmstudio/models/lmstudio-community/DeepSeek-R1-0528-Qwen3-8B-MLX-4bit"
+def pdf_to_b64(path: str) -> str:
+    with open(path, "rb") as f:
+        return base64.b64encode(f.read()).decode("utf-8")
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForCausalLM.from_pretrained(model_path)
-pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer, device=device)
 
-chain = PromptTemplate.from_template(
-    "Write me an algorithm for {topic} in Go."
-) | HuggingFacePipeline(pipeline=pipeline)
-response = chain.invoke({"topic": "Binary search"})
+def extract_pdf_text(path: str) -> str:
+    reader = PdfReader(path)
+    text = ""
+    for page in reader.pages:
+        if text.strip():
+            text += "\n\n"
+        text += page.extract_text()
+    print(f"Total tokens in the PDF file: {len(encoder.encode(text)):,}")
+    return text
 
+
+llm = ChatOpenAI(base_url="http://127.0.0.1:8089/v1")
+chain = (
+    RunnablePassthrough.assign(
+        pdf_file=lambda _: extract_pdf_text("./tmp/oai_gpt-oss_model_card.pdf")
+    )
+    | ChatPromptTemplate.from_messages(
+        [
+            SystemMessagePromptTemplate.from_template(
+                "You are an helpful assistant who helps the user in regular tasks.\n\n**Information you must take into account when helping the user:**\n{pdf_file}\n\n"
+            ),
+            HumanMessagePromptTemplate.from_template("{input}"),
+        ]
+    )
+    | llm
+)
+
+response = chain.invoke(
+    {"input": "Give me a brief comparison between gpt-oss-20b and gpt-oss-120b."}
+)
 print(response)
